8f41100d628f63314e06ac33b87823d727d7c9a6
Ronaldj11 <ronald.pineda01@uptc.edu.co>
2022-06-28 01:10:52 -0500

    cambios yalm Swagger

--- src/app.js
+++ src/app.js
@@ -4,7 +4,15 @@ import morgan from "morgan";
 import usersRoutes from "./routes/users.routes";
 import subjectsRoutes from "./routes/subjects.routes";
 
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerJSDoc = YAML.load('./api.yaml')
+
+
 const app = express();
+
+
+
 
 // Settings
 app.set("port", 4000);
@@ -14,7 +22,11 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 // Routes 
+
 app.use("/api/users", usersRoutes);
 app.use("/api/subjects", subjectsRoutes);
+
+// swagger
+app.use('/api-doc', swaggerUI.serve, swaggerUI.setup(swaggerJSDoc));
 
 export default app;
