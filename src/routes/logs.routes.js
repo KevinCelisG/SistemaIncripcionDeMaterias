e0d6ae26a317a5ef37569cf343739cfec28152da
keos14 <53238499+keos14@users.noreply.github.com>
2022-07-26 12:35:35 -0500

    Update logs.routes.js

--- src/routes/logs.routes.js
+++ src/routes/logs.routes.js
@@ -1,1 +1,10 @@
+import { Router } from "express";
+import { methods as logController } from "../controllers/log.controller";
 
+const router = Router();
+
+router.get("/", userController.getLogs);
+router.post("/", userController.addLog);
+router.get("/:id", userController.getLog);
+
+export default router;
