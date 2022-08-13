ead65fb28e8645523b19936c6aa81de33cfef15a
Henry Bonilla <duvan-bonilla@hotmail.com>
2022-08-12 22:21:13 -0500

    prisma db pull

--- src/app.js
+++ src/app.js
@@ -18,6 +18,15 @@ const app = express();
 // Settings
 app.set("port", 4000);
 
+//cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 // Middlewares - Details about request and response
 app.use(morgan("dev"));
 app.use(express.json());
